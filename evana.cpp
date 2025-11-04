1f00e11eb04d0b441ba2218f51861d0e0c277a4c
Rainer <evan.r.rusly@gmail.com>
2025-11-04 20:23:19 +0700

    Revert "Add initial structure for vision processing in evana.cpp"
    This reverts commit 195db8d71efa03eecf6d30da79831f9b7c58136b.

--- evana.cpp
+++
@@ -1,6 +0,0 @@
-/vision
-  HandTracker.kt           // init & run MediaPipe Hand Landmarker
-  GestureFeatures.kt       // ekstraksi fitur landmark → fitur jari/gerak
-  GestureEngine.kt         // rules + debounce + state machine
-  OverlayView.kt           // gambar landmark/vektor/FPS
-  VisionConfig.kt          // threshold, cooldown, numHands, GPU toggle
